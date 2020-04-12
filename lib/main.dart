82dcaaaeba3e1d272c72e6aaa60ed077f0238347
srikanth7785 <vanamalasrikanth7785@gmail.com>
2020-04-12 13:25:43 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+
+import 'package:upi_india/upi_india.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  Future _transaction;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  Future<String> initiateTransaction(String app) async {
+    var upiId = app == "UpiIndiaApps.GooglePay" ? 'srinivas134095149@okhdfcbank' : '8464881676@ybl' ;
+    var amount = app == "UpiIndiaApps.GooglePay" ? 10.00 : 15.00;
+
+    UpiIndia upi = new UpiIndia(
+      app: app,
+      receiverUpiId: upiId,
+      receiverName: 'Vanamala Srinivas',
+      transactionRefId: '7785',
+      transactionNote: 'Not actual. Just an example.',
+      amount: amount,
+    );
+
+    String response = await upi.startTransaction();
+
+    return response;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Phone Pay"),
+                  onPressed: (){
+                    _transaction = initiateTransaction(UpiIndiaApps.PhonePe).then((v){
+                      print("\n\nTRANSACTION RESPONSE IS : $v\n\n");
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Google Pay"),
+                  onPressed: (){
+                    _transaction = initiateTransaction(UpiIndiaApps.GooglePay).then((v){
+                      print("\n\nTRANSACTION RESPONSE IS : $v\n\n");
+                    });
+                  },
+                ),
+              ],
+            ),
+
+          SizedBox(
+            height: 25.0,
+          ),
+            
+          Container(
+            child: FutureBuilder(
+              future: _transaction,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting ||
+                    snapshot.data == null)
+                  return Text('Payment Not Initiated');
+                else {
+                  switch (snapshot.data.toString()) {
+                    case UpiIndiaResponseError.APP_NOT_INSTALLED:
+                      return Text(
+                        'App not installed.',
+                      );
+                      break;
+                    case UpiIndiaResponseError.INVALID_PARAMETERS:
+                      return Text(
+                        'App is unable to handle request.',
+                      );
+                      break;
+                    case UpiIndiaResponseError.USER_CANCELLED:
+                      return Text(
+                        'It seems like you cancelled the transaction.',
+                      );
+                      break;
+                    case UpiIndiaResponseError.NULL_RESPONSE:
+                      return Text(
+                        'No data received',
+                      );
+                      break;
+                    default:
+                      UpiIndiaResponse _upiResponse;
+                      _upiResponse = UpiIndiaResponse(snapshot.data);
+                      String txnId = _upiResponse.transactionId;
+                      String resCode = _upiResponse.responseCode;
+                      String txnRef = _upiResponse.transactionRefId;
+                      String status = _upiResponse.status;
+                      String approvalRef = _upiResponse.approvalRefNo;
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text('Transaction Id: $txnId'),
+                          Text('Response Code: $resCode'),
+                          Text('Reference Id: $txnRef'),
+                          Text('Status: $status'),
+                          Text('Approval No: $approvalRef'),
+                        ],
+                      );
+                  }
+                }
+              },
+            ),
+          )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
